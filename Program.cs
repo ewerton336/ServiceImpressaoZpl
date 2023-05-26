55f5a5045318f251241235cd9da1f6bc0ed5adf2
Ewerton Guimarães <ewerton.guimaraes@becomex.com.br>
2023-05-26 10:27:43 -0300

    Projeto inicial

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+string diretorio = @"C:\Users\ewerton.guimaraes\Desktop\Etiquetas";
+string diretorioImpressos = Path.Combine(diretorio, "Impressos");
+
+try
+{
+    // Criar a pasta "Impressos" se ainda não existir
+    if (!Directory.Exists(diretorioImpressos))
+    {
+        Directory.CreateDirectory(diretorioImpressos);
+    }
+
+    // Obter todos os arquivos com extensão .txt no diretório
+    string[] arquivosZPL = Directory.GetFiles(diretorio, "*.txt");
+
+    foreach (string arquivoZPL in arquivosZPL)
+    {
+        // Ler o conteúdo do arquivo ZPL
+        string conteudoZPL = File.ReadAllText(arquivoZPL, Encoding.UTF8);
+
+        // Enviar o conteúdo para a impressora
+        RawPrinterHelper.SendStringToPrinter("NomeDaImpressora", conteudoZPL);
+
+        // Obter o nome do arquivo apenas
+        string nomeArquivo = Path.GetFileName(arquivoZPL);
+
+        // Mover o arquivo para a pasta "Impressos"
+        string destino = Path.Combine(diretorioImpressos, nomeArquivo);
+        File.Move(arquivoZPL, destino);
+
+        Console.WriteLine($"Impressão do arquivo {arquivoZPL} concluída e movido para a pasta Impressos.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocorreu um erro durante a impressão: " + ex.Message);
+}
---
+++ RawPrinterHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class RawPrinterHelper
+{
+    [DllImport("winspool.drv", CharSet = CharSet.Unicode, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool OpenPrinter(string pPrinterName, out IntPtr phPrinter, IntPtr pDefault);
+
+    [DllImport("winspool.drv", CharSet = CharSet.Unicode, ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool StartDocPrinter(IntPtr hPrinter, int level, [In] ref DOCINFOA pDocInfo);
+
+    [DllImport("winspool.drv", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool EndDocPrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.drv", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool StartPagePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.drv", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool EndPagePrinter(IntPtr hPrinter);
+
+    [DllImport("winspool.drv", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool WritePrinter(IntPtr hPrinter, IntPtr pBytes, int dwCount, out int dwWritten);
+
+    [DllImport("winspool.drv", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.StdCall)]
+    public static extern bool ClosePrinter(IntPtr hPrinter);
+
+    public static bool SendStringToPrinter(string printerName, string data)
+    {
+        IntPtr pBytes;
+        int dwCount;
+        int dwWritten = 0;
+
+        dwCount = data.Length;
+        pBytes = Marshal.StringToCoTaskMemUni(data);
+
+        IntPtr hPrinter;
+        DOCINFOA di = new DOCINFOA();
+        bool success = false;
+
+        di.pDocName = "Raw Printer Data";
+        di.pDataType = "RAW";
+
+        if (OpenPrinter(printerName.Normalize(), out hPrinter, IntPtr.Zero))
+        {
+            if (StartDocPrinter(hPrinter, 1, ref di))
+            {
+                if (StartPagePrinter(hPrinter))
+                {
+                    success = WritePrinter(hPrinter, pBytes, dwCount, out dwWritten);
+                    EndPagePrinter(hPrinter);
+                }
+                EndDocPrinter(hPrinter);
+            }
+            ClosePrinter(hPrinter);
+        }
+
+        Marshal.FreeCoTaskMem(pBytes);
+        return success;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public class DOCINFOA
+    {
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string pDocName;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string pOutputFile;
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string pDataType;
+    }
+}
